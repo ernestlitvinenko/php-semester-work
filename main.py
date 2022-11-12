828d665f110ec7ec16ec3903f88d4ff4f3548b13
Ernest Litvinenko <ernest.litvinenko@mail.ru>
2022-11-12 09:38:09 +0300

    Add docker-compose file run server add environment variables

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+from os import environ
+import uvicorn
 from fastapi import FastAPI
-
+from database import database_init
+from dotenv import load_dotenv
 app = FastAPI()
 
+# On startup
+@app.on_event('startup')
+def statrup_handler():
+    load_dotenv('development.env')
+    print("Connection string is", environ.get('MONGO_DB_CONNECTION_STRING'))
+    
+    # set attribute database for app var  
+
+    setattr(app, 'database',  database_init(environ.get('MONGO_DB_CONNECTION_STRING', '')))
+
 if __name__ == '__main__':
-    print('Hello world!')
+    uvicorn.run('main:app', port=8000, host='127.0.0.1')
+            
